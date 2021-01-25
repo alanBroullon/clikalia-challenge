85ae0097309990663eec01b28029b06c15aedbe1
alanbo <alanozzu@gmail.com>
2021-01-25 09:49:29 +0100

    firebase config fixed

--- firebase.config.js
+++ firebase.config.js
@@ -4,13 +4,13 @@ import 'firebase/firestore';
 const loadFirebase = () => {
   try {
     const config = {
-      apiKey: process.env.FIREBASE_API_KEY,
-      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-      databaseURL: process.env.FIREBASE_DATABASE,
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-      messagingSenderId: process.env.FIREBASE_SENDER_ID,
-      appId: process.env.FIREBASE_APP_ID
+      apiKey: 'AIzaSyBluUC7l_n_wsxi2nDrSOQ9WXhhF2izGsk',
+      authDomain: 'petapp-a6695.firebaseapp.com',
+      databaseURL: 'https://petapp-a6695.firebaseapp.com',
+      projectId: 'petapp-a6695',
+      storageBucket: 'petapp-a6695.appspot.com',
+      messagingSenderId: '746467191954',
+      appId: '1:746467191954:web:5848fe808eadc2e90d0a4b'
     };
     firebase.initializeApp(config);
   } catch (error) {
